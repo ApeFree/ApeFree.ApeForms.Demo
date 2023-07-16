336a5f1a5a874067cf9f965917f92365536a1b63
Guijie Lee <landriesnidis@yeah.net>
2023-07-16 22:44:22 +0800

    更换图标

--- ApeFree.ApeForms.Demo/DemoForm.Designer.cs
+++ ApeFree.ApeForms.Demo/DemoForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DemoForm));
             this.SuspendLayout();
             // 
             // DemoForm
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DemoForm";
             this.Text = "DemoForm";
             this.ResumeLayout(false);
