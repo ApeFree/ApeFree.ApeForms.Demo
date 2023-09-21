e4a78580c2a8a3d7d05a4c776c1474cf81526f4b
Guijie Lee <landriesnidis@yeah.net>
2023-09-22 05:28:15 +0800

    更新Toast示例：支持修改字体、背景色、前景色

--- ApeFree.ApeForms.Demo/DemoPanel/ToastDemoPanel.Designer.cs
+++ ApeFree.ApeForms.Demo/DemoPanel/ToastDemoPanel.Designer.cs
@@ -31,59 +31,142 @@ namespace ApeFree.ApeForms.Demo.DemoPanel
         private void InitializeComponent()
         {
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.btnShowToast = new SimpleButton();
+            this.comboBoxLocation = new System.Windows.Forms.ComboBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnToastForeColor = new ApeFree.ApeForms.Core.Controls.SimpleButton();
+            this.btnToastFont = new ApeFree.ApeForms.Core.Controls.SimpleButton();
+            this.btnToastBackColor = new ApeFree.ApeForms.Core.Controls.SimpleButton();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.trackBarToastDelay = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
+            this.tbtnToastMode2 = new System.Windows.Forms.RadioButton();
+            this.tbtnToastMode1 = new System.Windows.Forms.RadioButton();
             this.tbtnToastMode0 = new System.Windows.Forms.RadioButton();
+            this.btnShowToast = new ApeFree.ApeForms.Core.Controls.SimpleButton();
             this.labToastCount = new System.Windows.Forms.Label();
             this.tbToastContent = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.tbtnToastMode1 = new System.Windows.Forms.RadioButton();
-            this.tbtnToastMode2 = new System.Windows.Forms.RadioButton();
             this.groupBox3.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarToastDelay)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.comboBoxLocation);
+            this.groupBox3.Controls.Add(this.panel3);
+            this.groupBox3.Controls.Add(this.panel2);
+            this.groupBox3.Controls.Add(this.panel1);
             this.groupBox3.Controls.Add(this.btnShowToast);
-            this.groupBox3.Controls.Add(this.label5);
-            this.groupBox3.Controls.Add(this.trackBarToastDelay);
-            this.groupBox3.Controls.Add(this.label4);
-            this.groupBox3.Controls.Add(this.tbtnToastMode0);
             this.groupBox3.Controls.Add(this.labToastCount);
             this.groupBox3.Controls.Add(this.tbToastContent);
             this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Controls.Add(this.tbtnToastMode1);
-            this.groupBox3.Controls.Add(this.tbtnToastMode2);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox3.Location = new System.Drawing.Point(0, 0);
             this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox3.Size = new System.Drawing.Size(651, 242);
+            this.groupBox3.Size = new System.Drawing.Size(651, 317);
             this.groupBox3.TabIndex = 9;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Toast";
             // 
-            // btnShowToast
+            // comboBoxLocation
             // 
-            this.btnShowToast.AutoSize = true;
-            this.btnShowToast.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
-            this.btnShowToast.ForeColor = System.Drawing.Color.White;
-            this.btnShowToast.Location = new System.Drawing.Point(5, 56);
-            this.btnShowToast.Name = "btnShowToast";
-            this.btnShowToast.Size = new System.Drawing.Size(91, 37);
-            this.btnShowToast.TabIndex = 22;
-            this.btnShowToast.Title = "Show";
-            this.btnShowToast.Click += new System.EventHandler(this.btnShowToast_Click);
+            this.comboBoxLocation.FormattingEnabled = true;
+            this.comboBoxLocation.Location = new System.Drawing.Point(288, 31);
+            this.comboBoxLocation.Name = "comboBoxLocation";
+            this.comboBoxLocation.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxLocation.TabIndex = 26;
+            this.comboBoxLocation.Visible = false;
+            this.comboBoxLocation.SelectedIndexChanged += new System.EventHandler(this.comboBoxLocation_SelectedIndexChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnToastForeColor);
+            this.panel3.Controls.Add(this.btnToastFont);
+            this.panel3.Controls.Add(this.btnToastBackColor);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(2, 118);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(647, 52);
+            this.panel3.TabIndex = 25;
+            // 
+            // btnToastForeColor
+            // 
+            this.btnToastForeColor.BackColor = System.Drawing.Color.White;
+            this.btnToastForeColor.BorderColor = System.Drawing.Color.Empty;
+            this.btnToastForeColor.BorderSize = 1;
+            this.btnToastForeColor.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnToastForeColor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnToastForeColor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnToastForeColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToastForeColor.ForeColor = System.Drawing.Color.Black;
+            this.btnToastForeColor.Location = new System.Drawing.Point(109, 3);
+            this.btnToastForeColor.Name = "btnToastForeColor";
+            this.btnToastForeColor.Size = new System.Drawing.Size(100, 35);
+            this.btnToastForeColor.TabIndex = 2;
+            this.btnToastForeColor.Text = "Toast ForeColor";
+            this.btnToastForeColor.UseVisualStyleBackColor = false;
+            this.btnToastForeColor.Click += new System.EventHandler(this.btnToastForeColor_Click);
+            // 
+            // btnToastFont
+            // 
+            this.btnToastFont.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnToastFont.BorderColor = System.Drawing.Color.Empty;
+            this.btnToastFont.BorderSize = 0;
+            this.btnToastFont.FlatAppearance.BorderSize = 0;
+            this.btnToastFont.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(132)))), ((int)(((byte)(236)))));
+            this.btnToastFont.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(96)))), ((int)(((byte)(172)))));
+            this.btnToastFont.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(144)))), ((int)(((byte)(255)))));
+            this.btnToastFont.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToastFont.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnToastFont.Location = new System.Drawing.Point(215, 3);
+            this.btnToastFont.Name = "btnToastFont";
+            this.btnToastFont.Size = new System.Drawing.Size(100, 35);
+            this.btnToastFont.TabIndex = 1;
+            this.btnToastFont.Text = "Toast Font";
+            this.btnToastFont.UseVisualStyleBackColor = false;
+            this.btnToastFont.Click += new System.EventHandler(this.btnToastFont_Click);
+            // 
+            // btnToastBackColor
+            // 
+            this.btnToastBackColor.BackColor = System.Drawing.Color.Gray;
+            this.btnToastBackColor.BorderColor = System.Drawing.Color.Empty;
+            this.btnToastBackColor.BorderSize = 0;
+            this.btnToastBackColor.FlatAppearance.BorderSize = 0;
+            this.btnToastBackColor.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(140)))), ((int)(((byte)(140)))));
+            this.btnToastBackColor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.btnToastBackColor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.btnToastBackColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToastBackColor.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnToastBackColor.Location = new System.Drawing.Point(3, 3);
+            this.btnToastBackColor.Name = "btnToastBackColor";
+            this.btnToastBackColor.Size = new System.Drawing.Size(100, 35);
+            this.btnToastBackColor.TabIndex = 0;
+            this.btnToastBackColor.Text = "Toast BackColor";
+            this.btnToastBackColor.UseVisualStyleBackColor = false;
+            this.btnToastBackColor.Click += new System.EventHandler(this.btnToastBackColor_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.trackBarToastDelay);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(2, 170);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(647, 69);
+            this.panel2.TabIndex = 24;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.label5.Location = new System.Drawing.Point(2, 123);
+            this.label5.Location = new System.Drawing.Point(0, 12);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(59, 12);
@@ -93,36 +176,89 @@ namespace ApeFree.ApeForms.Demo.DemoPanel
             // trackBarToastDelay
             // 
             this.trackBarToastDelay.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.trackBarToastDelay.Location = new System.Drawing.Point(2, 135);
+            this.trackBarToastDelay.Location = new System.Drawing.Point(0, 24);
             this.trackBarToastDelay.Minimum = 1;
             this.trackBarToastDelay.Name = "trackBarToastDelay";
             this.trackBarToastDelay.Size = new System.Drawing.Size(647, 45);
             this.trackBarToastDelay.TabIndex = 20;
             this.trackBarToastDelay.Value = 1;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tbtnToastMode2);
+            this.panel1.Controls.Add(this.tbtnToastMode1);
+            this.panel1.Controls.Add(this.tbtnToastMode0);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(2, 239);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(647, 76);
+            this.panel1.TabIndex = 23;
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.label4.Location = new System.Drawing.Point(2, 180);
+            this.label4.Location = new System.Drawing.Point(0, 16);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(131, 12);
             this.label4.TabIndex = 19;
             this.label4.Text = "ToastMode（显示模式）";
             // 
+            // tbtnToastMode2
+            // 
+            this.tbtnToastMode2.AutoSize = true;
+            this.tbtnToastMode2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbtnToastMode2.Location = new System.Drawing.Point(0, 28);
+            this.tbtnToastMode2.Name = "tbtnToastMode2";
+            this.tbtnToastMode2.Size = new System.Drawing.Size(647, 16);
+            this.tbtnToastMode2.TabIndex = 18;
+            this.tbtnToastMode2.Text = "Reuse - 复用模式：清除队列，使用当前正在显示的Toast";
+            this.tbtnToastMode2.UseVisualStyleBackColor = true;
+            // 
+            // tbtnToastMode1
+            // 
+            this.tbtnToastMode1.AutoSize = true;
+            this.tbtnToastMode1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbtnToastMode1.Location = new System.Drawing.Point(0, 44);
+            this.tbtnToastMode1.Name = "tbtnToastMode1";
+            this.tbtnToastMode1.Size = new System.Drawing.Size(647, 16);
+            this.tbtnToastMode1.TabIndex = 17;
+            this.tbtnToastMode1.Text = "Preemption - 抢占模式：清除队列，下一次弹出时显示";
+            this.tbtnToastMode1.UseVisualStyleBackColor = true;
+            // 
             // tbtnToastMode0
             // 
             this.tbtnToastMode0.AutoSize = true;
             this.tbtnToastMode0.Checked = true;
             this.tbtnToastMode0.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tbtnToastMode0.Location = new System.Drawing.Point(2, 192);
+            this.tbtnToastMode0.Location = new System.Drawing.Point(0, 60);
             this.tbtnToastMode0.Name = "tbtnToastMode0";
             this.tbtnToastMode0.Size = new System.Drawing.Size(647, 16);
             this.tbtnToastMode0.TabIndex = 16;
             this.tbtnToastMode0.TabStop = true;
             this.tbtnToastMode0.Text = "Queue - 队列模式：消息加入队列，顺序显示";
             this.tbtnToastMode0.UseVisualStyleBackColor = true;
+            // 
+            // btnShowToast
+            // 
+            this.btnShowToast.AutoSize = true;
+            this.btnShowToast.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnShowToast.BorderColor = System.Drawing.Color.Empty;
+            this.btnShowToast.BorderSize = 1;
+            this.btnShowToast.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(134)))), ((int)(((byte)(224)))));
+            this.btnShowToast.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(97)))), ((int)(((byte)(163)))));
+            this.btnShowToast.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(146)))), ((int)(((byte)(244)))));
+            this.btnShowToast.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShowToast.ForeColor = System.Drawing.Color.White;
+            this.btnShowToast.Location = new System.Drawing.Point(5, 56);
+            this.btnShowToast.Name = "btnShowToast";
+            this.btnShowToast.Size = new System.Drawing.Size(100, 35);
+            this.btnShowToast.TabIndex = 22;
+            this.btnShowToast.Text = "Show";
+            this.btnShowToast.UseVisualStyleBackColor = false;
+            this.btnShowToast.Click += new System.EventHandler(this.btnShowToast_Click);
             // 
             // labToastCount
             // 
@@ -155,38 +291,21 @@ namespace ApeFree.ApeForms.Demo.DemoPanel
             this.label3.TabIndex = 4;
             this.label3.Text = "Content:";
             // 
-            // tbtnToastMode1
-            // 
-            this.tbtnToastMode1.AutoSize = true;
-            this.tbtnToastMode1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tbtnToastMode1.Location = new System.Drawing.Point(2, 208);
-            this.tbtnToastMode1.Name = "tbtnToastMode1";
-            this.tbtnToastMode1.Size = new System.Drawing.Size(647, 16);
-            this.tbtnToastMode1.TabIndex = 17;
-            this.tbtnToastMode1.Text = "Preemption - 抢占模式：清除队列，下一次弹出时显示";
-            this.tbtnToastMode1.UseVisualStyleBackColor = true;
-            // 
-            // tbtnToastMode2
-            // 
-            this.tbtnToastMode2.AutoSize = true;
-            this.tbtnToastMode2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tbtnToastMode2.Location = new System.Drawing.Point(2, 224);
-            this.tbtnToastMode2.Name = "tbtnToastMode2";
-            this.tbtnToastMode2.Size = new System.Drawing.Size(647, 16);
-            this.tbtnToastMode2.TabIndex = 18;
-            this.tbtnToastMode2.Text = "Reuse - 复用模式：清除队列，使用当前正在显示的Toast";
-            this.tbtnToastMode2.UseVisualStyleBackColor = true;
-            // 
-            // NotificationDemoPanel
+            // ToastDemoPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.groupBox3);
-            this.Name = "NotificationDemoPanel";
+            this.Name = "ToastDemoPanel";
             this.Size = new System.Drawing.Size(651, 436);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarToastDelay)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -204,5 +323,12 @@ namespace ApeFree.ApeForms.Demo.DemoPanel
         private System.Windows.Forms.RadioButton tbtnToastMode1;
         private System.Windows.Forms.RadioButton tbtnToastMode2;
         private SimpleButton btnShowToast;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private SimpleButton btnToastBackColor;
+        private System.Windows.Forms.ComboBox comboBoxLocation;
+        private SimpleButton btnToastFont;
+        private SimpleButton btnToastForeColor;
     }
 }
--- ApeFree.ApeForms.Demo/DemoPanel/ToastDemoPanel.cs
+++ ApeFree.ApeForms.Demo/DemoPanel/ToastDemoPanel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ApeFree.ApeForms.Demo.DemoPanel
 {
@@ -17,6 +18,9 @@ namespace ApeFree.ApeForms.Demo.DemoPanel
         public ToastDemoPanel()
         {
             InitializeComponent();
+
+            comboBoxLocation.Items.AddRange(Enum.GetNames(typeof(ToastLocation)));
+            comboBoxLocation.SelectedIndex = 0;
         }
 
         private void btnShowToast_Click(object sender, EventArgs e)
@@ -26,6 +30,49 @@ namespace ApeFree.ApeForms.Demo.DemoPanel
 
             Toast.Show($"{tbToastContent.Text} - {++toastCount}", trackBarToastDelay.Value * 500, tbToastContent, mode);
             labToastCount.Text = toastCount + "";
+        }
+
+        private void btnToastBackColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            dialog.AllowFullOpen = false;
+            dialog.ShowHelp = true;
+            dialog.Color = Toast.BackColor;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                btnToastBackColor.BackColor = Toast.BackColor = dialog.Color;
+            }
+        }
+
+        private void btnToastForeColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            dialog.AllowFullOpen = false;
+            dialog.ShowHelp = true;
+            dialog.Color = Toast.BackColor;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                btnToastForeColor.ForeColor = Toast.ForeColor = dialog.Color;
+            }
+        }
+
+        private void btnToastFont_Click(object sender, EventArgs e)
+        {
+            FontDialog dialog = new FontDialog();
+            dialog.ShowColor = true;
+            dialog.Font = Toast.Font;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                btnToastFont.Font = Toast.Font = dialog.Font;
+            }
+        }
+
+        private void comboBoxLocation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Toast.Location = (ToastLocation)comboBoxLocation.SelectedIndex;
         }
     }
 }
