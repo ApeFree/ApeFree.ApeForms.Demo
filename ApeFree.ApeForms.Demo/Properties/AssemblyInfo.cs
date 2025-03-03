1e63025fb588ab548ed72663cbd432247801a384
Guijie Lee <landriesnidis@yeah.net>
2025-03-03 10:39:40 +0800

    更新Demo版本号

--- ApeFree.ApeForms.Demo/Properties/AssemblyInfo.cs
+++ ApeFree.ApeForms.Demo/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.5.1.1206")]
+[assembly: AssemblyFileVersion("1.5.2.0303")]
